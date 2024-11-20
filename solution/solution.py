63f04df772820a56545ffc79b042693794717ed4
Dmitry <dimitri.87@mail.ru>
2024-11-20 18:22:59 +0500

    first commit

--- solution/solution.py
+++ solution/solution.py
@@ -1,44 +1,31 @@
 def appearance(intervals: dict[str, list[int]]) -> int:
     lesson = intervals['lesson']
+
     pupil = []
     for i in range(1, len(intervals['pupil']), 2):
         res = search_overlap([intervals['pupil'][i-1], intervals['pupil'][i]], lesson)
         if range(res[0], res[1]):
             pupil.append(res[0])
             pupil.append(res[1])
-    print(pupil)
+
     tutor = []
     for i in range(1, len(intervals['tutor']), 2):
         res = search_overlap([intervals['tutor'][i - 1], intervals['tutor'][i]], lesson)
         if range(res[0], res[1]):
             tutor.append(res[0])
             tutor.append(res[1])
-    print(tutor)
+
     sum_ = []
     for i in range(1, len(pupil), 2):
         for j in range(1, len(tutor), 2):
             res = search_overlap([pupil[i - 1], pupil[i]], [tutor[j - 1], tutor[j]])
             if range(res[0], res[1]):
                 sum_ += list(range(res[0], res[1]))
-    print(len(set(sum_)))
 
-
-def replace_time(lesson: list[int, int], person: list[int]) -> list[int]:
-
-    for i in range(len(person)):
-        if person[i] < min(lesson):
-            person[i] = min(lesson)
-        elif person[i] > max(lesson):
-            person[i] = max(lesson)
-    return person
+    return len(set(sum_))
 
 
 def search_overlap(range1: list[int, int], range2: list[int, int]):
-    # print(range1, range2, end='     ')
-    # print(max(range1[0], range2[0]), min(range1[1], range2[1]), end='   ')
-    #
-    # res = range(max(range1[0], range2[0]), min(range1[1], range2[1]))
-    # print(len(res))
     return max(range1[0], range2[0]), min(range1[1], range2[1])
 
 
@@ -61,7 +48,6 @@ tests = [
 ]
 
 if __name__ == '__main__':
-    appearance(tests[0]['intervals'])
-    # for i, test in enumerate(tests):
-    #     test_answer = appearance(test['intervals'])
-    #     assert test_answer == test['answer'], f'Error on test case {i}, got {test_answer}, expected {test["answer"]}'
+    for i, test in enumerate(tests):
+        test_answer = appearance(test['intervals'])
+        assert test_answer == test['answer'], f'Error on test case {i}, got {test_answer}, expected {test["answer"]}'
